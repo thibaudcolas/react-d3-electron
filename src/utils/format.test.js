dea7a6e1be19408b7471a8ef51823f44fec01624
Thibaud Colas <thibaudcolas@gmail.com>
2018-02-20 13:02:38 +0200

    Upgrade test framework to Jest

--- src/utils/format.test.js
+++ src/utils/format.test.js
@@ -1,43 +1,43 @@
-import { humanNumber, padNumber, monthName } from '../utils/format';
+import { humanNumber, padNumber, monthName } from "../utils/format";
 
-describe('format', () => {
-    describe('#humanNumber', () => {
-        it('exists', () => {
-            expect(humanNumber).to.be.a('function');
-        });
-
-        it('number formatting makes the numbers look nicer', () => {
-            expect(humanNumber(0)).to.equal('0');
-            expect(humanNumber(5000)).to.equal('5,000');
-            expect(humanNumber(5000000)).to.equal('5,000,000');
-            expect(humanNumber(-5000)).to.equal('-5,000');
-            expect(humanNumber(-5000000)).to.equal('-5,000,000');
-            expect(humanNumber(5000.5)).to.equal('5,000.5');
-            expect(humanNumber(0.3333333)).to.equal('0.3333333');
-        });
+describe("format", () => {
+  describe("#humanNumber", () => {
+    it("exists", () => {
+      expect(humanNumber).toBeInstanceOf(Function);
     });
 
-    describe('#padNumber', () => {
-        it('exists', () => {
-            expect(padNumber).to.be.a('function');
-        });
+    it("number formatting makes the numbers look nicer", () => {
+      expect(humanNumber(0)).toBe("0");
+      expect(humanNumber(5000)).toBe("5,000");
+      expect(humanNumber(5000000)).toBe("5,000,000");
+      expect(humanNumber(-5000)).toBe("-5,000");
+      expect(humanNumber(-5000000)).toBe("-5,000,000");
+      expect(humanNumber(5000.5)).toBe("5,000.5");
+      expect(humanNumber(0.3333333)).toBe("0.3333333");
+    });
+  });
 
-        it('pads single-digit numbers with a zero', () => {
-            expect(padNumber(0)).to.equal('00');
-            expect(padNumber(9)).to.equal('09');
-            expect(padNumber(-1)).to.equal('-1');
-            expect(padNumber(0.3333333)).to.equal('00.3333333');
-        });
+  describe("#padNumber", () => {
+    it("exists", () => {
+      expect(padNumber).toBeInstanceOf(Function);
     });
 
-    describe('#monthName', () => {
-        it('exists', () => {
-            expect(monthName).to.be.a('function');
-        });
-
-        it('converts a month number to a string, from 0 to 11', () => {
-            expect(monthName(0)).to.equal('January');
-            expect(monthName(11)).to.equal('December');
-        });
+    it("pads single-digit numbers with a zero", () => {
+      expect(padNumber(0)).toBe("00");
+      expect(padNumber(9)).toBe("09");
+      expect(padNumber(-1)).toBe("-1");
+      expect(padNumber(0.3333333)).toBe("00.3333333");
     });
+  });
+
+  describe("#monthName", () => {
+    it("exists", () => {
+      expect(monthName).toBeInstanceOf(Function);
+    });
+
+    it("converts a month number to a string, from 0 to 11", () => {
+      expect(monthName(0)).toBe("January");
+      expect(monthName(11)).toBe("December");
+    });
+  });
 });
